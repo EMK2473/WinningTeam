e8cc050d7017a4b0d77d642be42b5624332f76fd
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-30 17:24:12 -0400

    declared private fields; defined blank methods required

--- Team.java
+++ Team.java
@@ -1,18 +1,45 @@
 public class Team {
     // TODO: Declare private fields - name, wins, losses
-    
+    private String name;
+    private int wins;
+    private int losses;
     
     // TODO: Define mutator methods - 
     //       setName(), setWins(), setLosses()
+    public static void setName() {
+      
+    }
+    
+    public static void setWins(){
+       
+    }
+    
+    public static void setLosses(){
+       
+    }
     
     
     // TODO: Define accessor methods - 
     //       getName(), getWins(), getLosses()
+    public static getName(){
+      
+    }
     
+    public static getWins() {
+       
+    }
+    
+    public static getLosses() {
+       
+    }
     
     // TODO: Define getWinPercentage()
-    
+    public static getWinPercentage(){
+       
+    }
     
     // TODO: Define printStanding()
-    
+    public static printStanding() {
+       
+    }
  }
